ee37f65fa06b12f4fd1a109434850de90ca13574
Nemanja Jevtić <njevtic22@gmail.com>
2024-10-29 21:55:36 +0100

    Code refactoring

---
+++ studentservice/src/main/java/com/example/studentservice/command/AuthenticatedCommand.java
@@ -0,0 +1,82 @@
+package com.example.studentservice.command;
+
+import com.example.studentservice.menu.Menu;
+import com.example.studentservice.model.Role;
+import com.example.studentservice.model.User;
+import com.example.studentservice.security.AuthenticationService;
+import com.example.studentservice.util.Colors;
+import com.example.studentservice.util.ConsoleReader;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Qualifier("authenticated-command")
+public class AuthenticatedCommand implements Command {
+    private final AuthenticationService service;
+    private final List<Command> adminCommands;
+    private final List<Command> referentCommands;
+    private final ConsoleReader console;
+    private final Menu menu;
+
+    public AuthenticatedCommand(
+            AuthenticationService service,
+            @CommandGroup("admin-menu") List<Command> adminCommands,
+            @CommandGroup("referent-menu") List<Command> referentCommands,
+            ConsoleReader console,
+            Menu menu
+    ) {
+        this.service = service;
+        this.adminCommands = adminCommands;
+        this.referentCommands = referentCommands;
+        this.console = console;
+        this.menu = menu;
+    }
+
+    @Override
+    public void execute() {
+        User authenticated = (User) service.getAuthenticated();
+        // TODO: separate this in another class
+        List<Command> chosenCommands = getChosenCommands(authenticated.getRole());
+
+        System.out.println("\n" + authenticated.getName() + " " + authenticated.getSurname() + " successfully logged in");
+        while (true) {
+            System.out.println("Following commands are available:");
+            menu.printCommands(chosenCommands);
+            System.out.println("0. Log out");
+
+            int input = console.nextInt("Enter number of desired command: ");
+            System.out.println();
+            if (input > 0 && input <= chosenCommands.size()) {
+                chosenCommands.get(input - 1).execute();
+            } else if (input == 0) {
+                service.invalidateAuthentication();
+                System.out.println(Colors.likeWarning("Logged out"));
+                return;
+            } else {
+                System.out.println(Colors.likeError("Invalid option"));
+            }
+            System.out.println();
+        }
+    }
+
+    @Override
+    public String getDescription() {
+        return "";
+    }
+
+    private List<Command> getChosenCommands(Role role) {
+//        Assumes there are only two roles and if more roles are added later than it will return referentCommands
+//        as oppose the chosen solution which will throw error
+//        return role == Role.ADMIN ? adminCommands : referentCommands;
+
+        if (role == Role.ADMIN) {
+            return adminCommands;
+        } else if (role == Role.REFERENT) {
+            return referentCommands;
+        } else {
+            throw new IllegalArgumentException("Role " + role.toString() + " is not supported");
+        }
+    }
+}
--- studentservice/src/main/java/com/example/studentservice/command/anonymous/LoginCommand.java
+++ studentservice/src/main/java/com/example/studentservice/command/anonymous/LoginCommand.java
@@ -1,40 +1,31 @@
 package com.example.studentservice.command.anonymous;
 
+import com.example.studentservice.command.AuthenticatedCommand;
 import com.example.studentservice.command.Command;
 import com.example.studentservice.command.CommandGroup;
-import com.example.studentservice.menu.Menu;
-import com.example.studentservice.model.Role;
 import com.example.studentservice.model.User;
 import com.example.studentservice.security.AuthenticationService;
-import com.example.studentservice.util.Colors;
 import com.example.studentservice.util.ConsoleReader;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 @Order(1)
 @CommandGroup("anonymous-menu")
 public class LoginCommand implements Command {
     private final AuthenticationService service;
-    private final List<Command> adminCommands;
-    private final List<Command> referentCommands;
+    private final AuthenticatedCommand authCommand;
     private final ConsoleReader console;
-    private final Menu menu;
 
     public LoginCommand(
             AuthenticationService service,
-            @CommandGroup("admin-menu") List<Command> adminCommands,
-            @CommandGroup("referent-menu") List<Command> referentCommands,
-            ConsoleReader console,
-            Menu menu
+            @Qualifier("authenticated-command") AuthenticatedCommand authCommand,
+            ConsoleReader console
     ) {
         this.service = service;
-        this.adminCommands = adminCommands;
-        this.referentCommands = referentCommands;
+        this.authCommand = authCommand;
         this.console = console;
-        this.menu = menu;
     }
 
     @Override
@@ -44,46 +35,11 @@ public class LoginCommand implements Command {
         // TODO: add try again
         User authenticated = (User) service.authenticate(username, password);
 
-        // TODO: separate this in another class
-        List<Command> chosenCommands = getChosenCommands(authenticated.getRole());
-
-        System.out.println("\n" + authenticated.getName() + " " + authenticated.getSurname() + " successfully logged in");
-        while (true) {
-            System.out.println("Following commands are available:");
-            menu.printCommands(chosenCommands);
-            System.out.println("0. Log out");
-
-            int input = console.nextInt("Enter number of desired command: ");
-            System.out.println();
-            if (input > 0 && input <= chosenCommands.size()) {
-                chosenCommands.get(input - 1).execute();
-            } else if (input == 0) {
-                service.invalidateAuthentication();
-                System.out.println(Colors.likeWarning("Logged out"));
-                return;
-            } else {
-                System.out.println(Colors.likeError("Invalid option"));
-            }
-            System.out.println();
-        }
+        authCommand.execute();
     }
 
     @Override
     public String getDescription() {
         return "Log in";
-    }
-
-    private List<Command> getChosenCommands(Role role) {
-//        Assumes there are only two roles and if more roles are added later than it will return referentCommands
-//        as oppose the chosen solution which will throw error
-//        return role == Role.ADMIN ? adminCommands : referentCommands;
-
-        if (role == Role.ADMIN) {
-            return adminCommands;
-        } else if (role == Role.REFERENT) {
-            return referentCommands;
-        } else {
-            throw new IllegalArgumentException("Role " + role.toString() + " is not supported");
-        }
     }
 }
